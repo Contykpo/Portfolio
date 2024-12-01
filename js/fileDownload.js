66b43f2bb91477676d486bd66966ac1fd8b424b5
Contykpo <samu.1furlanich@gmail.com>
2024-12-01 17:12:50 -0300

    Cleared repository

--- js/fileDownload.js
+++
@@ -1,7 +0,0 @@
-﻿window.triggerFileDownload = (fileName, url) => {
-    const anchorElement = document.createElement('a');
-    anchorElement.href = url;
-    anchorElement.download = fileName ?? '';
-    anchorElement.click();
-    anchorElement.remove();
-}
